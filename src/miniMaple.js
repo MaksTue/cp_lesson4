14a8f8020f42bcd0cc586d15490354d5327e4416
MaksTue <pro100max48@gmail.com>
2023-10-10 14:54:37 +0300

    add miniMapleand tests

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -1,5 +1,14 @@
-class MiniMaple{
-
+const math = require("mathjs");
+class MiniMaple {
+  static differentiate(expression, variable) {
+    const invalidOperators = expression.match(/[^+\-\*\^0-9a-zA-Z\s]/);
+    if (invalidOperators) {
+      throw new Error("Недопустимые операции в полиноме");
+    }
+    const x = math.parse(variable);
+    const polyExpression = math.parse(expression);
+    const derivative = math.derivative(polyExpression, x);
+    return derivative.toString().replace(/\s/g, "");
+  }
 }
-
-export {MiniMaple}
+export { MiniMaple };
--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,5 +1,22 @@
-import {MiniMaple} from "../src/miniMaple";
+import { MiniMaple } from "../src/miniMaple";
 
-test('it fails', () => {
-    expect(false).toBeTruthy();
+test("test Example", () => {
+  const polynomial = "4*x^3";
+  const variable = "x";
+  const derivative = MiniMaple.differentiate(polynomial, variable);
+  expect(derivative).toBe("12*x^2");
+});
+
+test("test Example", () => {
+  const polynomial = "4*x^3";
+  const variable = "y";
+  const derivative = MiniMaple.differentiate(polynomial, variable);
+  expect(derivative).toBe("0");
+});
+
+test("test Example", () => {
+  const polynomial = "4*x^3-x^2";
+  const variable = "x";
+  const derivative = MiniMaple.differentiate(polynomial, variable);
+  expect(derivative).toBe("12*x^2-2*x");
 });
